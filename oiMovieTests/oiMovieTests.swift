813c248f53bcb533a8f99d17f4ffb05b9a07ff0b
yerimbet.aitzhanov <king_erosh@icloud.com>
2024-11-12 17:07:30 +0500

    Initial Commit

---
+++ oiMovieTests/oiMovieTests.swift
@@ -0,0 +1,17 @@
+//
+//  oiMovieTests.swift
+//  oiMovieTests
+//
+//  Created by Ерош Айтжанов on 12.11.2024.
+//
+
+import Testing
+@testable import oiMovie
+
+struct oiMovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
